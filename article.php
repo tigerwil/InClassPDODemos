cea30dd28205f4eacac425b5e5850f1fe36b93b5
Marc Williams <oti_instructor@hotmail.com>
2018-12-11 09:25:18 -0400

    Add members only functionality

--- article.php
+++ article.php
@@ -2,5 +2,12 @@
     $page_title = 'Article';
     $current= "article";
     include 'includes/header.php';
-    include 'templates/article.php';
+    if(isset($_SESSION['user_id'])){
+        //user is logged in and account is not expired - show article
+         include 'templates/article.php';
+    }else{
+        //user is not logged in 
+        include 'templates/membersonly.php';
+    }  
+    
     include 'includes/footer.php';
---
+++ templates/membersonly.php
@@ -0,0 +1,18 @@
+<div class="container">
+   <!-- Page Heading/Breadcrumbs -->
+   <h1 class="mt-4 mb-3">Members Only</h1>
+   <ol class="breadcrumb">
+     <li class="breadcrumb-item">
+       <a href="index.html">Home</a>
+     </li>
+     <li class="breadcrumb-item active">Members Only</li>
+   </ol> 
+   <div class="alert alert-danger" role="alert">
+       <strong>Not Logged In</strong>
+       <p>You must be logged in as a registered user
+           to view the requested article</p>
+       <p><a href="login.php">Login</a></p>
+   </div>
+    
+</div>
+
